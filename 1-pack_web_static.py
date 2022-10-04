01e36957f6ac8a767eb391960b28189e8bb51800
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-10-04 18:42:28 +0300

    Hot fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,5 @@ from fabric.api import local
 
 def do_pack():
     """Function that generates a tar of /web_static"""
-    try:
-        local(
-                'tar -cvzf "versions/web_static_$(date \'+%Y%m%d%H%M%S\')".tar\
-                ./web_static')
-        return "versions/"
-    except Exception:
-        return None
+    local('tar -cvzf "versions/web_static_$(date \'+%Y%m%d%H%M%S\')".tar\
+            ./web_static')
