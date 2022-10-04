126c2ae1fa63b8b1ed1242c0986569c7a299c87c
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-10-04 19:14:47 +0300

    Hot fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,10 +2,18 @@
 """
 Contain a function that generates tar file of /web_static/ dir
 """
+import os.path
 from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
     """Function that generates a tar of /web_static"""
-    local('tar -cvzf "versions/web_static_$(date \'+%Y%m%d%H%M%S\')".tar\
-            ./web_static')
+    dt = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tar".format(dt)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local('tar -cvzf {} web_static'.format(file)).failed is True:
+        return None
+    return file
